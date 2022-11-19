8dc95cee741ae7f670d49325387dc545c8c83273
yongkyu <dopeboy0608@gmail.com>
2022-11-19 11:18:50 +0900

    test: 푸시테스트용 커밋

--- src/components/recoil-test/RecoilValue.tsx
+++ src/components/recoil-test/RecoilValue.tsx
@@ -5,7 +5,7 @@ import { charCountState } from '@/store/sample';
 const RecoilSelector: React.FC = () => {
   const inputValue = useRecoilValue(charCountState);
 
-  return <>출력 텍스트: {inputValue}</>;
+  return <>출력 텍스트1: {inputValue}</>;
 };
 
 export default memo(RecoilSelector);
