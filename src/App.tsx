d9f11cfa5b4e755829ede772091384beb3a8794b
yongkyu <dopeboy0608@gmail.com>
2022-11-19 10:49:38 +0900

    feat: 샘플 컴퍼넌트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,30 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Parent from '@/components/parent/Parent';
+import RecoilInput from '@/components/recoil-test/RecoilInput';
+import RecoilValue from '@/components/recoil-test/RecoilValue';
+// import Parent from '@/components/parent/Parent';
 
 function App(): any {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        {/* <p>
           Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
+        </p> */}
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
-        <Parent title="제목" onTitleClick={() => console.log('titleClicked')} />
+        </a> */}
+        {/* <Parent title="제목" onTitleClick={() => console.log('titleClicked')} /> */}
+        <p>리코일 테스트!!!</p>
+        <RecoilInput />
+        <RecoilValue />
       </header>
     </div>
   );
