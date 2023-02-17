e4c3fcd74b4f2d156bc9c868432b240b1ebe2b52
dopeboy0608 <dopeboy0608@gmail.com>
2023-02-17 23:50:03 +0900

    refactor: Router 컴퍼넌트 분리, lazyUtil 추가
    Co-authored-by: HyunKwan Roh <hkroh.nonoll@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,19 @@
-import React, { lazy, Suspense } from 'react';
-import { Routes, Route, Navigate, BrowserRouter } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import MainNav from '@/components/MainNav';
 import logo from './logo.svg';
 import './App.css';
-
-// lazy 사용시 Suspense 필요함.
-// const AxiosTest = lazy(() => import('@/pages/axios-test/AxiosTest'));
-// const EnvTest = lazy(() => import('@/pages/env-test/EnvTest'));
-// const RecoilTest = lazy(() => import('@/pages/recoil-test/RecoilTest'));
-
-import AxiosTest from '@/pages/axios-test/AxiosTest';
-import EnvTest from '@/pages/env-test/EnvTest';
-import RecoilTest from '@/pages/recoil-test/RecoilTest';
-import MainNav from '@/components/MainNav';
+import AppRouter from '@/routers/AppRouter';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        {/* <Suspense> */}
         <BrowserRouter>
           <MainNav />
-          <Routes>
-            <Route path="/axios-test" element={<AxiosTest />} />
-            <Route path="/env-test" element={<EnvTest />} />
-            <Route path="/recoil-test" element={<RecoilTest />} />
-            <Route path="*" element={<Navigate replace to="/axios-test" />} />
-          </Routes>
+          <AppRouter />
         </BrowserRouter>
-        {/* </Suspense> */}
       </header>
     </div>
   );
